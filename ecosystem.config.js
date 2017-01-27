57245b3dbfa306747ce9470205b16e88b9c6f8f6
Andrey Menshikh <andrey@light-tecnologies.com>
2017-01-27 23:02:38 +0300

    PM2 Ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [{
+    name: "Trials Statistic Dashboard",
+    script: "./dist/server.js",
+    watch: true,
+    env: {
+      NODE_ENV: "production"
+    },
+    env_production: {
+      NODE_ENV: "production"
+    },
+    instances: "max",
+    exec_mode: "cluster"
+  }]
+}
